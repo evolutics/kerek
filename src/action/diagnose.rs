80cb9fbc3491d31370564eb4d28ee9555f029657
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-06 23:00:42 +0200

    Implement diagnose to show dependency versions

--- src/action/diagnose.rs
+++ src/action/diagnose.rs
@@ -1,3 +1,14 @@
+use crate::library::run_command;
+use std::process;
+
 pub fn go() -> Result<(), String> {
-    todo!()
+    run_command::go(process::Command::new("git").arg("version"))?;
+
+    run_command::go(process::Command::new("skaffold").args([
+        "version",
+        "--output",
+        "Skaffold {{.Version}}\n",
+    ]))?;
+
+    run_command::go(process::Command::new("vagrant").arg("--version"))
 }

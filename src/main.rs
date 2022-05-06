b5ec5f1959ab281bffa4acfd8d141da58c1bd003
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-06 18:33:07 +0200

    Add subcommand stubs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+mod clean;
+mod inspect;
+mod run;
+
+use clap::Parser;
+
 fn main() {
-    println!("Hi");
+    let arguments = Arguments::parse();
+
+    match arguments.action {
+        Action::Clean => clean::go(),
+        Action::Inspect => inspect::go(),
+        Action::Run => run::go(),
+    }
+}
+
+#[derive(Parser)]
+#[clap(version)]
+struct Arguments {
+    #[clap(subcommand)]
+    action: Action,
+}
+
+#[derive(clap::Subcommand)]
+enum Action {
+    Clean,
+    Inspect,
+    Run,
 }

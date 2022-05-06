15801ccb162db22597ce0662749ae97a8fd98d53
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-06 22:03:38 +0200

    Implement clean to remove VM (including snapshots) and discard changes

--- src/action/clean.rs
+++ src/action/clean.rs
@@ -1,3 +1,16 @@
+use crate::library::constants;
+use crate::library::run_command;
+use std::process;
+
 pub fn go() -> Result<(), String> {
-    todo!()
+    if constants::vagrantfile().exists() {
+        run_command::go(
+            process::Command::new("vagrant")
+                .args(["destroy", "--force"])
+                .current_dir(constants::WORK_FOLDER),
+        )?
+    }
+
+    run_command::go(process::Command::new("git").args(["restore", "."]))?;
+    run_command::go(process::Command::new("git").args(["clean", "-d", "--force", "-x"]))
 }
--- src/library/run_command.rs
+++ src/library/run_command.rs
@@ -1,6 +1,5 @@
 use std::process;
 
-#[allow(dead_code)]
 pub fn go(command: &mut process::Command) -> Result<(), String> {
     match command.status() {
         Result::Err(error) => Result::Err(format!("{command:?}: {error}")),

e74b2016161df93e5b1e3c446e94049fbc0356bc
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-07 09:05:54 +0200

    Extract function

--- src/action/clear.rs
+++ src/action/clear.rs
@@ -3,14 +3,20 @@ use crate::library::run_command;
 use std::process;
 
 pub fn go() -> Result<(), String> {
+    remove_vm_if_exists()?;
+
+    run_command::go(process::Command::new("git").args(["restore", "."]))?;
+    run_command::go(process::Command::new("git").args(["clean", "-d", "--force", "-x"]))
+}
+
+fn remove_vm_if_exists() -> Result<(), String> {
     if constants::vagrantfile().exists() {
         run_command::go(
             process::Command::new("vagrant")
                 .args(["destroy", "--force"])
                 .current_dir(constants::WORK_FOLDER),
-        )?
+        )
+    } else {
+        Ok(())
     }
-
-    run_command::go(process::Command::new("git").args(["restore", "."]))?;
-    run_command::go(process::Command::new("git").args(["clean", "-d", "--force", "-x"]))
 }

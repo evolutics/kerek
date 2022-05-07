a670e2b50c093afbd6ee9d1e137de16ce583ce0b
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-07 11:49:35 +0200

    Extract for reuse

--- looper/src/library/constants.rs
+++ looper/src/library/constants.rs
@@ -1,3 +1,9 @@
+use std::path;
+
 pub const CONFIGURATION_FILE: &str = "kerek.json";
 
 pub const WORK_FOLDER: &str = ".kerek";
+
+pub fn vagrantfile() -> path::PathBuf {
+    [WORK_FOLDER, "Vagrantfile"].iter().collect()
+}

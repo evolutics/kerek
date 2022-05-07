e682e64c23de19e57229a9fb0b5b385bc15a9966
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-07 09:12:16 +0200

    Remove only own files as too dangerous otherwise

--- src/action/clear.rs
+++ src/action/clear.rs
@@ -1,13 +1,12 @@
 use crate::library::constants;
 use crate::library::run_command;
+use std::fs;
 use std::path;
 use std::process;
 
 pub fn go() -> Result<(), String> {
     remove_vm_if_exists()?;
-
-    run_command::go(process::Command::new("git").args(["restore", "."]))?;
-    run_command::go(process::Command::new("git").args(["clean", "-d", "--force", "-x"]))
+    remove_work_folder_if_exists()
 }
 
 fn remove_vm_if_exists() -> Result<(), String> {
@@ -21,6 +20,14 @@ fn remove_vm_if_exists() -> Result<(), String> {
                 .args(["destroy", "--force"])
                 .current_dir(constants::WORK_FOLDER),
         )
+    } else {
+        Ok(())
+    }
+}
+
+fn remove_work_folder_if_exists() -> Result<(), String> {
+    if path::Path::new(constants::WORK_FOLDER).exists() {
+        fs::remove_dir_all(constants::WORK_FOLDER).map_err(|error| format!("{error}"))
     } else {
         Ok(())
     }

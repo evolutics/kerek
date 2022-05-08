6ab5b57b086c04464b9766b4d2a2759cb12f79d2
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-08 11:14:17 +0200

    Extract cleaning function for reuse

--- src/action/clean.rs
+++ src/action/clean.rs
@@ -1,29 +1,5 @@
-use crate::library::constants;
-use crate::library::run_command;
-use std::fs;
-use std::path;
-use std::process;
+use crate::library::clean;
 
 pub fn go() -> anyhow::Result<()> {
-    remove_vm_if_exists()?;
-    remove_work_folder_if_exists()
-}
-
-fn remove_vm_if_exists() -> anyhow::Result<()> {
-    if constants::vagrantfile().exists() {
-        run_command::go(
-            process::Command::new("vagrant")
-                .args(["destroy", "--force"])
-                .current_dir(constants::WORK_FOLDER),
-        )
-    } else {
-        Ok(())
-    }
-}
-
-fn remove_work_folder_if_exists() -> anyhow::Result<()> {
-    if path::Path::new(constants::WORK_FOLDER).exists() {
-        fs::remove_dir_all(constants::WORK_FOLDER)?;
-    }
-    Ok(())
+    clean::go()
 }
---
+++ src/library/clean.rs
@@ -0,0 +1,29 @@
+use super::constants;
+use super::run_command;
+use std::fs;
+use std::path;
+use std::process;
+
+pub fn go() -> anyhow::Result<()> {
+    remove_vm_if_exists()?;
+    remove_work_folder_if_exists()
+}
+
+fn remove_vm_if_exists() -> anyhow::Result<()> {
+    if constants::vagrantfile().exists() {
+        run_command::go(
+            process::Command::new("vagrant")
+                .args(["destroy", "--force"])
+                .current_dir(constants::WORK_FOLDER),
+        )
+    } else {
+        Ok(())
+    }
+}
+
+fn remove_work_folder_if_exists() -> anyhow::Result<()> {
+    if path::Path::new(constants::WORK_FOLDER).exists() {
+        fs::remove_dir_all(constants::WORK_FOLDER)?;
+    }
+    Ok(())
+}

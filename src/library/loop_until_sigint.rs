0f488115cebc6f33c08cbaa59443c4404dba4e0f
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-08 13:01:40 +0200

    Implement indefinite loop with cleanup, halting on SIGINT (Ctrl+C)

---
+++ src/library/loop_until_sigint.rs
@@ -0,0 +1,26 @@
+use anyhow::Context;
+use std::sync::mpsc;
+
+#[allow(dead_code)]
+pub fn go<F: Fn() -> anyhow::Result<()>, G: Fn()>(iterate: F, clean_up: G) -> anyhow::Result<()> {
+    let (sender, receiver) = mpsc::channel();
+
+    ctrlc::set_handler(move || {
+        eprintln!("SIGINT acknowledged.");
+        sender.send(()).expect("Unable to send to channel.");
+    })?;
+
+    let result = loop {
+        match receiver.try_recv() {
+            Err(mpsc::TryRecvError::Empty) => (),
+            result => break result.context("Unable to receive from channel."),
+        }
+        let result = iterate();
+        if result.is_err() {
+            break result;
+        }
+    };
+
+    clean_up();
+    result
+}

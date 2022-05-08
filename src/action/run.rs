eb53dbbed32df84cae7e8ff39c3a512d0f64951f
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-08 14:12:09 +0200

    Start staging VM

--- src/action/run.rs
+++ src/action/run.rs
@@ -2,17 +2,24 @@ use crate::library::clean;
 use crate::library::configuration;
 use crate::library::constants;
 use crate::library::loop_until_sigint;
+use crate::library::run_command;
 use anyhow::Context;
 use std::fs;
+use std::process;
 
 pub fn go() -> anyhow::Result<()> {
     let configuration = configuration::get()?;
 
     loop_until_sigint::go(
-        set_up_work_folder,
+        set_up,
         || iterate(&configuration),
         || clean::go().expect("Unable to clean."),
     )
+}
+
+fn set_up() -> anyhow::Result<()> {
+    set_up_work_folder()?;
+    start_staging_vm()
 }
 
 fn set_up_work_folder() -> anyhow::Result<()> {
@@ -21,6 +28,15 @@ fn set_up_work_folder() -> anyhow::Result<()> {
         .with_context(|| format!("Unable to create folder, consider cleaning: {path}"))?;
     fs::write(constants::vagrantfile(), include_str!("Vagrantfile"))?;
     Ok(())
+}
+
+fn start_staging_vm() -> anyhow::Result<()> {
+    run_command::go(
+        process::Command::new("vagrant")
+            .arg("up")
+            .current_dir(constants::WORK_FOLDER)
+            .env("KEREK_IP", constants::STAGING_IP),
+    )
 }
 
 fn iterate(configuration: &configuration::Data) -> anyhow::Result<()> {
--- src/library/constants.rs
+++ src/library/constants.rs
@@ -2,6 +2,8 @@ use std::path;
 
 pub const CONFIGURATION_FILE: &str = "kerek.json";
 
+pub const STAGING_IP: &str = "192.168.63.63";
+
 pub const WORK_FOLDER: &str = ".kerek";
 
 pub fn vagrantfile() -> path::PathBuf {

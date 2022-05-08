b08dac166aff0aa8eb8cfcaf612e4d355cede4e0
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-08 18:16:55 +0200

    Run local Bash script on remote machine via SSH for provisioning

---
+++ looper/src/library/run_bash_script_over_ssh.rs
@@ -0,0 +1,19 @@
+use super::run_command;
+use std::fs;
+use std::process;
+
+#[allow(dead_code)]
+pub fn go(in_: In) -> anyhow::Result<()> {
+    let script = fs::File::open(in_.script_file)?;
+    run_command::go(
+        process::Command::new("ssh")
+            .args(["-F", in_.configuration_file, in_.hostname, "bash"])
+            .stdin(script),
+    )
+}
+
+pub struct In<'a> {
+    pub configuration_file: &'a str,
+    pub hostname: &'a str,
+    pub script_file: &'a str,
+}

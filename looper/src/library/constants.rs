cb2fa2703e548d95658a21c5bb7cc8c9d18cb0c0
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-08 17:37:10 +0200

    Use plain string as easy to treat as path but not vice versa

--- looper/src/library/constants.rs
+++ looper/src/library/constants.rs
@@ -1,5 +1,3 @@
-use std::path;
-
 pub const CONFIGURATION_FILE: &str = "kerek.json";
 
 pub const STAGING_IP: &str = "192.168.63.63";
@@ -8,6 +6,6 @@ pub const VAGRANTFILE: &str = include_str!("Vagrantfile");
 
 pub const WORK_FOLDER: &str = ".kerek";
 
-pub fn vagrantfile_file() -> path::PathBuf {
-    [WORK_FOLDER, "Vagrantfile"].iter().collect()
+pub fn vagrantfile_file() -> String {
+    format!("{WORK_FOLDER}/Vagrantfile")
 }

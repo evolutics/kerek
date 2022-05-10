621e8444d8d82af1e8ecce6db9e41f64202d518c
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-10 22:53:05 +0200

    Use more opinionated test categories

--- src/library/configuration.rs
+++ src/library/configuration.rs
@@ -13,7 +13,9 @@ pub fn get() -> anyhow::Result<Main> {
 
 pub struct Main {
     pub provision_extras: String,
-    pub test_base: String,
+    pub base_test: String,
+    pub acceptance_test: String,
+    pub smoke_test: String,
     pub staging: EnvironmentConfiguration,
     pub production: EnvironmentConfiguration,
 }
@@ -23,7 +25,6 @@ pub struct EnvironmentConfiguration {
     pub ssh_host: String,
     pub kubeconfig_file: String,
     pub public_ip: String,
-    pub test_file: String,
 }
 
 #[derive(serde::Deserialize)]
@@ -32,12 +33,12 @@ struct UserFacingConfiguration {
     #[serde(default = "default_provision_extras")]
     pub provision_extras: String,
 
-    #[serde(default = "default_test_base")]
-    pub test_base: String,
-    #[serde(default = "default_test_staging")]
-    pub test_staging: String,
-    #[serde(default = "default_test_production")]
-    pub test_production: String,
+    #[serde(default = "default_base_test")]
+    pub base_test: String,
+    #[serde(default = "default_acceptance_test")]
+    pub acceptance_test: String,
+    #[serde(default = "default_smoke_test")]
+    pub smoke_test: String,
 
     #[serde(default = "default_production_ssh_configuration")]
     pub production_ssh_configuration: String,
@@ -51,16 +52,16 @@ fn default_provision_extras() -> String {
     String::from("scripts/provision_extras.sh")
 }
 
-fn default_test_base() -> String {
-    String::from("scripts/test_base.sh")
+fn default_base_test() -> String {
+    String::from("scripts/base_test.sh")
 }
 
-fn default_test_staging() -> String {
-    String::from("scripts/test_staging.sh")
+fn default_acceptance_test() -> String {
+    String::from("scripts/acceptance_test.sh")
 }
 
-fn default_test_production() -> String {
-    String::from("scripts/test_production.sh")
+fn default_smoke_test() -> String {
+    String::from("scripts/smoke_test.sh")
 }
 
 fn default_production_ssh_configuration() -> String {
@@ -77,20 +78,20 @@ impl From<UserFacingConfiguration> for Main {
 
         Self {
             provision_extras: configuration.provision_extras,
-            test_base: configuration.test_base,
+            base_test: configuration.base_test,
+            acceptance_test: configuration.acceptance_test,
+            smoke_test: configuration.smoke_test,
             staging: EnvironmentConfiguration {
                 ssh_configuration_file: format!("{work_folder}/ssh_configuration"),
                 ssh_host: String::from("default"),
                 kubeconfig_file: format!("{work_folder}/kubeconfig"),
                 public_ip: String::from("192.168.63.63"),
-                test_file: configuration.test_staging,
             },
             production: EnvironmentConfiguration {
                 ssh_configuration_file: configuration.production_ssh_configuration,
                 ssh_host: configuration.production_ssh_host,
                 kubeconfig_file: configuration.production_kubeconfig,
                 public_ip: configuration.production_public_ip,
-                test_file: configuration.test_production,
             },
         }
     }

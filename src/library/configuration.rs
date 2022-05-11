09bbc33b769935d602fffc36a3a7ec7044de0082
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-11 22:58:38 +0200

    Turn into normal function to allow further arguments

--- src/library/configuration.rs
+++ src/library/configuration.rs
@@ -6,9 +6,8 @@ use std::path;
 
 pub fn get(path: path::PathBuf) -> anyhow::Result<Main> {
     let file = fs::File::open(&path).with_context(|| format!("Unable to open file: {path:?}"))?;
-    let configuration =
-        serde_json::from_reader::<_, UserFacingConfiguration>(io::BufReader::new(file))?;
-    Ok(configuration.into())
+    let configuration = serde_json::from_reader(io::BufReader::new(file))?;
+    Ok(convert(configuration))
 }
 
 pub struct Main {
@@ -42,43 +41,41 @@ struct UserFacingConfiguration {
     pub public_ip: String,
 }
 
-impl From<UserFacingConfiguration> for Main {
-    fn from(configuration: UserFacingConfiguration) -> Self {
-        Self {
-            provisioning_scripts: [
-                Some(constants::provision_base_file()),
-                configuration.provision_extras,
-            ]
-            .into_iter()
-            .flatten()
-            .collect(),
-            base_test: configuration
-                .base_test
-                .unwrap_or_else(|| ["scripts", "base_test.sh"].into_iter().collect()),
-            acceptance_test: configuration
-                .acceptance_test
-                .unwrap_or_else(|| ["scripts", "acceptance_test.sh"].into_iter().collect()),
-            smoke_test: configuration
-                .smoke_test
-                .unwrap_or_else(|| ["scripts", "smoke_test.sh"].into_iter().collect()),
-            staging: EnvironmentConfiguration {
-                ssh_configuration_file: [constants::WORK_FOLDER, "ssh_configuration"]
-                    .into_iter()
-                    .collect(),
-                ssh_host: String::from("default"),
-                kubeconfig_file: [constants::WORK_FOLDER, "kubeconfig"].into_iter().collect(),
-                public_ip: String::from("192.168.63.63"),
-            },
-            production: EnvironmentConfiguration {
-                ssh_configuration_file: configuration
-                    .ssh_configuration
-                    .unwrap_or_else(|| ["safe", "ssh_configuration"].into_iter().collect()),
-                ssh_host: configuration.ssh_host,
-                kubeconfig_file: configuration
-                    .kubeconfig
-                    .unwrap_or_else(|| ["safe", "kubeconfig"].into_iter().collect()),
-                public_ip: configuration.public_ip,
-            },
-        }
+fn convert(configuration: UserFacingConfiguration) -> Main {
+    Main {
+        provisioning_scripts: [
+            Some(constants::provision_base_file()),
+            configuration.provision_extras,
+        ]
+        .into_iter()
+        .flatten()
+        .collect(),
+        base_test: configuration
+            .base_test
+            .unwrap_or_else(|| ["scripts", "base_test.sh"].into_iter().collect()),
+        acceptance_test: configuration
+            .acceptance_test
+            .unwrap_or_else(|| ["scripts", "acceptance_test.sh"].into_iter().collect()),
+        smoke_test: configuration
+            .smoke_test
+            .unwrap_or_else(|| ["scripts", "smoke_test.sh"].into_iter().collect()),
+        staging: EnvironmentConfiguration {
+            ssh_configuration_file: [constants::WORK_FOLDER, "ssh_configuration"]
+                .into_iter()
+                .collect(),
+            ssh_host: String::from("default"),
+            kubeconfig_file: [constants::WORK_FOLDER, "kubeconfig"].into_iter().collect(),
+            public_ip: String::from("192.168.63.63"),
+        },
+        production: EnvironmentConfiguration {
+            ssh_configuration_file: configuration
+                .ssh_configuration
+                .unwrap_or_else(|| ["safe", "ssh_configuration"].into_iter().collect()),
+            ssh_host: configuration.ssh_host,
+            kubeconfig_file: configuration
+                .kubeconfig
+                .unwrap_or_else(|| ["safe", "kubeconfig"].into_iter().collect()),
+            public_ip: configuration.public_ip,
+        },
     }
 }

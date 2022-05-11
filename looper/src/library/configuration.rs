cbcad9b4d12bff6ed005a6cf0f05c8e1ba7505ae
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-11 21:52:32 +0200

    Move defaults to conversion for simplicity

--- looper/src/library/configuration.rs
+++ looper/src/library/configuration.rs
@@ -32,39 +32,14 @@ pub struct EnvironmentConfiguration {
 struct UserFacingConfiguration {
     pub provision_extras: Option<String>,
 
-    #[serde(default = "default_base_test")]
-    pub base_test: String,
-    #[serde(default = "default_acceptance_test")]
-    pub acceptance_test: String,
-    #[serde(default = "default_smoke_test")]
-    pub smoke_test: String,
+    pub base_test: Option<String>,
+    pub acceptance_test: Option<String>,
+    pub smoke_test: Option<String>,
 
-    #[serde(default = "default_ssh_configuration")]
-    pub ssh_configuration: String,
+    pub ssh_configuration: Option<String>,
     pub ssh_host: String,
-    #[serde(default = "default_kubeconfig")]
-    pub kubeconfig: String,
+    pub kubeconfig: Option<String>,
     pub public_ip: String,
-}
-
-fn default_base_test() -> String {
-    String::from("scripts/base_test.sh")
-}
-
-fn default_acceptance_test() -> String {
-    String::from("scripts/acceptance_test.sh")
-}
-
-fn default_smoke_test() -> String {
-    String::from("scripts/smoke_test.sh")
-}
-
-fn default_ssh_configuration() -> String {
-    String::from("safe/ssh_configuration")
-}
-
-fn default_kubeconfig() -> String {
-    String::from("safe/kubeconfig")
 }
 
 impl From<UserFacingConfiguration> for Main {
@@ -79,9 +54,15 @@ impl From<UserFacingConfiguration> for Main {
             .into_iter()
             .flatten()
             .collect(),
-            base_test: configuration.base_test,
-            acceptance_test: configuration.acceptance_test,
-            smoke_test: configuration.smoke_test,
+            base_test: configuration
+                .base_test
+                .unwrap_or_else(|| String::from("scripts/base_test.sh")),
+            acceptance_test: configuration
+                .acceptance_test
+                .unwrap_or_else(|| String::from("scripts/acceptance_test.sh")),
+            smoke_test: configuration
+                .smoke_test
+                .unwrap_or_else(|| String::from("scripts/smoke_test.sh")),
             staging: EnvironmentConfiguration {
                 ssh_configuration_file: format!("{work_folder}/ssh_configuration"),
                 ssh_host: String::from("default"),
@@ -89,9 +70,13 @@ impl From<UserFacingConfiguration> for Main {
                 public_ip: String::from("192.168.63.63"),
             },
             production: EnvironmentConfiguration {
-                ssh_configuration_file: configuration.ssh_configuration,
+                ssh_configuration_file: configuration
+                    .ssh_configuration
+                    .unwrap_or_else(|| String::from("safe/ssh_configuration")),
                 ssh_host: configuration.ssh_host,
-                kubeconfig_file: configuration.kubeconfig,
+                kubeconfig_file: configuration
+                    .kubeconfig
+                    .unwrap_or_else(|| String::from("safe/kubeconfig")),
                 public_ip: configuration.public_ip,
             },
         }

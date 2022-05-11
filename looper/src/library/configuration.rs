ff61b69bad47cc70845155ad890de764947ec7d0
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-11 22:16:19 +0200

    Use path types for clarity

--- looper/src/library/configuration.rs
+++ looper/src/library/configuration.rs
@@ -2,6 +2,7 @@ use super::constants;
 use anyhow::Context;
 use std::fs;
 use std::io;
+use std::path;
 
 pub fn get() -> anyhow::Result<Main> {
     let path = constants::CONFIGURATION_FILE;
@@ -12,40 +13,38 @@ pub fn get() -> anyhow::Result<Main> {
 }
 
 pub struct Main {
-    pub provisioning_scripts: Vec<String>,
-    pub base_test: String,
-    pub acceptance_test: String,
-    pub smoke_test: String,
+    pub provisioning_scripts: Vec<path::PathBuf>,
+    pub base_test: path::PathBuf,
+    pub acceptance_test: path::PathBuf,
+    pub smoke_test: path::PathBuf,
     pub staging: EnvironmentConfiguration,
     pub production: EnvironmentConfiguration,
 }
 
 pub struct EnvironmentConfiguration {
-    pub ssh_configuration_file: String,
+    pub ssh_configuration_file: path::PathBuf,
     pub ssh_host: String,
-    pub kubeconfig_file: String,
+    pub kubeconfig_file: path::PathBuf,
     pub public_ip: String,
 }
 
 #[derive(serde::Deserialize)]
 #[serde(deny_unknown_fields)]
 struct UserFacingConfiguration {
-    pub provision_extras: Option<String>,
+    pub provision_extras: Option<path::PathBuf>,
 
-    pub base_test: Option<String>,
-    pub acceptance_test: Option<String>,
-    pub smoke_test: Option<String>,
+    pub base_test: Option<path::PathBuf>,
+    pub acceptance_test: Option<path::PathBuf>,
+    pub smoke_test: Option<path::PathBuf>,
 
-    pub ssh_configuration: Option<String>,
+    pub ssh_configuration: Option<path::PathBuf>,
     pub ssh_host: String,
-    pub kubeconfig: Option<String>,
+    pub kubeconfig: Option<path::PathBuf>,
     pub public_ip: String,
 }
 
 impl From<UserFacingConfiguration> for Main {
     fn from(configuration: UserFacingConfiguration) -> Self {
-        let work_folder = constants::WORK_FOLDER;
-
         Self {
             provisioning_scripts: [
                 Some(constants::provision_base_file()),
@@ -56,27 +55,29 @@ impl From<UserFacingConfiguration> for Main {
             .collect(),
             base_test: configuration
                 .base_test
-                .unwrap_or_else(|| String::from("scripts/base_test.sh")),
+                .unwrap_or_else(|| ["scripts", "base_test.sh"].into_iter().collect()),
             acceptance_test: configuration
                 .acceptance_test
-                .unwrap_or_else(|| String::from("scripts/acceptance_test.sh")),
+                .unwrap_or_else(|| ["scripts", "acceptance_test.sh"].into_iter().collect()),
             smoke_test: configuration
                 .smoke_test
-                .unwrap_or_else(|| String::from("scripts/smoke_test.sh")),
+                .unwrap_or_else(|| ["scripts", "smoke_test.sh"].into_iter().collect()),
             staging: EnvironmentConfiguration {
-                ssh_configuration_file: format!("{work_folder}/ssh_configuration"),
+                ssh_configuration_file: [constants::WORK_FOLDER, "ssh_configuration"]
+                    .into_iter()
+                    .collect(),
                 ssh_host: String::from("default"),
-                kubeconfig_file: format!("{work_folder}/kubeconfig"),
+                kubeconfig_file: [constants::WORK_FOLDER, "kubeconfig"].into_iter().collect(),
                 public_ip: String::from("192.168.63.63"),
             },
             production: EnvironmentConfiguration {
                 ssh_configuration_file: configuration
                     .ssh_configuration
-                    .unwrap_or_else(|| String::from("safe/ssh_configuration")),
+                    .unwrap_or_else(|| ["safe", "ssh_configuration"].into_iter().collect()),
                 ssh_host: configuration.ssh_host,
                 kubeconfig_file: configuration
                     .kubeconfig
-                    .unwrap_or_else(|| String::from("safe/kubeconfig")),
+                    .unwrap_or_else(|| ["safe", "kubeconfig"].into_iter().collect()),
                 public_ip: configuration.public_ip,
             },
         }
--- looper/src/library/run_bash_script_over_ssh.rs
+++ looper/src/library/run_bash_script_over_ssh.rs
@@ -1,18 +1,22 @@
 use super::run_command;
 use std::fs;
+use std::path;
 use std::process;
 
 pub fn go(in_: In) -> anyhow::Result<()> {
     let script = fs::File::open(in_.script_file)?;
     run_command::go(
         process::Command::new("ssh")
-            .args(["-F", in_.configuration_file, in_.host, "bash"])
+            .arg("-F")
+            .arg(in_.configuration_file)
+            .arg(in_.host)
+            .arg("bash")
             .stdin(script),
     )
 }
 
 pub struct In<'a> {
-    pub configuration_file: &'a str,
+    pub configuration_file: &'a path::Path,
     pub host: &'a str,
-    pub script_file: &'a str,
+    pub script_file: &'a path::Path,
 }

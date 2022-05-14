e2300e263f683d15c5740b01bf2b8bd1c28202f9
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-14 09:53:43 +0200

    Clean before instead of after for simplicity

--- looper/src/action/run/mod.rs
+++ looper/src/action/run/mod.rs
@@ -1,17 +1,14 @@
 mod iterate;
 mod set_up;
 
-use crate::library::clean;
 use crate::library::configuration;
-use crate::library::loop_until_sigint;
 use std::path;
 
 pub fn go(configuration: path::PathBuf) -> anyhow::Result<()> {
     let configuration = configuration::get(configuration)?;
+    set_up::go(&configuration)?;
 
-    loop_until_sigint::go(loop_until_sigint::In {
-        set_up: || set_up::go(&configuration),
-        iterate: || iterate::go(&configuration),
-        tear_down: || clean::go(&configuration.work_folder).expect("Unable to clean."),
-    })
+    loop {
+        iterate::go(&configuration)?;
+    }
 }

9c3498201ee57d6b8e48f94be80f9a373c1789a9
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-14 10:16:06 +0200

    Make setting up idempotent to use it without tearing down first

--- looper/src/library/set_up_workspace.rs
+++ looper/src/library/set_up_workspace.rs
@@ -4,7 +4,7 @@ use std::fs;
 use std::path;
 
 pub fn go(work_folder: &path::Path) -> anyhow::Result<()> {
-    fs::create_dir(work_folder)
+    fs::create_dir_all(work_folder)
         .with_context(|| format!("Unable to create folder: {work_folder:?}"))?;
     for (filename, contents) in [
         (assets::PROVISION_BASE_FILENAME, assets::PROVISION_BASE),

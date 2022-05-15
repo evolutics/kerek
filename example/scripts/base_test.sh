2ca6d98001d3ffd50c8d5c33721d824fbbe800de
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-15 19:54:59 +0200

    Use single quotes if no variable interpolation needed

--- example/scripts/base_test.sh
+++ example/scripts/base_test.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "Base testing" >>log.txt
+echo 'Base testing' >>log.txt
--- example/scripts/smoke_test.sh
+++ example/scripts/smoke_test.sh
@@ -4,10 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "Smoke testing" >>log.txt
+echo 'Smoke testing' >>log.txt
 
 curl --connect-timeout 3 --fail --retry 2 --show-error http://"$1":8080
 
-if [[ "$1" == "192.168.62.62" ]]; then
+if [[ "$1" == '192.168.62.62' ]]; then
   exit 1
 fi

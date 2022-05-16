b52fd8c5ef83d29b1dfce8487327943a6266592d
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-16 17:55:58 +0200

    Do not rebase configured paths for simplicity

--- src/library/configuration.rs
+++ src/library/configuration.rs
@@ -6,10 +6,7 @@ use std::path;
 pub fn get(path: path::PathBuf) -> anyhow::Result<Main> {
     let file = fs::File::open(&path).with_context(|| format!("Unable to open file: {path:?}"))?;
     let configuration = serde_json::from_reader(io::BufReader::new(file))?;
-    let root = path
-        .parent()
-        .ok_or_else(|| anyhow::anyhow!("No parent for path: {path:?}"))?;
-    Ok(convert(configuration, root))
+    Ok(convert(configuration))
 }
 
 pub struct Main {
@@ -67,52 +64,40 @@ struct UserFacingProductionConfiguration {
     pub public_ip: String,
 }
 
-fn convert(configuration: UserFacingConfiguration, root: &path::Path) -> Main {
-    let workspace_folder = root.join(
-        configuration
-            .workspace_folder
-            .unwrap_or_else(|| path::PathBuf::from(".kerek")),
-    );
+fn convert(configuration: UserFacingConfiguration) -> Main {
+    let workspace_folder = configuration
+        .workspace_folder
+        .unwrap_or_else(|| path::PathBuf::from(".kerek"));
     let staging = staging_configuration(&workspace_folder);
     let workspace = workspace_configuration(workspace_folder);
 
     Main {
         workspace,
         tests: TestsConfiguration {
-            base: root.join(
-                configuration
-                    .tests
-                    .base
-                    .unwrap_or_else(|| ["scripts", "base_test.sh"].iter().collect()),
-            ),
-            acceptance: root.join(
-                configuration
-                    .tests
-                    .acceptance
-                    .unwrap_or_else(|| ["scripts", "acceptance_test.sh"].iter().collect()),
-            ),
-            smoke: root.join(
-                configuration
-                    .tests
-                    .smoke
-                    .unwrap_or_else(|| ["scripts", "smoke_test.sh"].iter().collect()),
-            ),
+            base: configuration
+                .tests
+                .base
+                .unwrap_or_else(|| ["scripts", "base_test.sh"].iter().collect()),
+            acceptance: configuration
+                .tests
+                .acceptance
+                .unwrap_or_else(|| ["scripts", "acceptance_test.sh"].iter().collect()),
+            smoke: configuration
+                .tests
+                .smoke
+                .unwrap_or_else(|| ["scripts", "smoke_test.sh"].iter().collect()),
         },
         staging,
         production: EnvironmentConfiguration {
-            ssh_configuration_file: root.join(
-                configuration
-                    .production
-                    .ssh_configuration
-                    .unwrap_or_else(|| ["safe", "ssh_configuration"].iter().collect()),
-            ),
+            ssh_configuration_file: configuration
+                .production
+                .ssh_configuration
+                .unwrap_or_else(|| ["safe", "ssh_configuration"].iter().collect()),
             ssh_host: configuration.production.ssh_host,
-            kubeconfig_file: root.join(
-                configuration
-                    .production
-                    .kubeconfig
-                    .unwrap_or_else(|| ["safe", "kubeconfig"].iter().collect()),
-            ),
+            kubeconfig_file: configuration
+                .production
+                .kubeconfig
+                .unwrap_or_else(|| ["safe", "kubeconfig"].iter().collect()),
             public_ip: configuration.production.public_ip,
         },
     }

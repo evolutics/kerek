f8f1f513c29cc35fe4b098bc59511f1a45f388b0
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-17 20:46:26 +0200

    Pass script as literal string instead of filename for simplicity

--- looper/src/library/set_up_workspace.rs
+++ looper/src/library/set_up_workspace.rs
@@ -6,10 +6,6 @@ pub fn go(workspace: &configuration::WorkspaceConfiguration) -> anyhow::Result<(
     let folder = &workspace.folder;
     fs::create_dir_all(folder).with_context(|| format!("Unable to create folder: {folder:?}"))?;
     for (file, contents) in [
-        (
-            &workspace.move_to_next_version,
-            include_str!("assets/move_to_next_version.sh"),
-        ),
         (&workspace.provision, include_str!("assets/provision.sh")),
         (&workspace.vagrantfile, include_str!("assets/Vagrantfile")),
     ] {

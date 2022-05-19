5b6c5ec8b29ea80fccc37639ae2216a00187285a
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-19 22:23:36 +0200

    Group code for clarity

--- src/library/set_up_cache.rs
+++ src/library/set_up_cache.rs
@@ -5,11 +5,13 @@ use std::fs;
 pub fn go(cache: &configuration::Cache) -> anyhow::Result<()> {
     let folder = &cache.folder;
     fs::create_dir_all(folder).with_context(|| format!("Unable to create folder: {folder:?}"))?;
+
     for (file, contents) in [
         (&cache.provision, include_str!("assets/provision.sh")),
         (&cache.vagrantfile, include_str!("assets/Vagrantfile")),
     ] {
         fs::write(file, contents)?;
     }
+
     Ok(())
 }

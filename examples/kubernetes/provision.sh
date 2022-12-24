eb0e33dfceec592043fab17131e9ee228a136d83
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-12-24 16:46:05 +0100

    Repeat waiting to prevent race condition if there is no node yet
    If there is no node when calling the wait command, it immediately fails
    with "error: no matching resources found".

--- examples/kubernetes/provision.sh
+++ examples/kubernetes/provision.sh
@@ -6,7 +6,10 @@ set -o pipefail
 
 ssh -F "${KEREK_SSH_CONFIGURATION}" "${KEREK_SSH_HOST}" -- '
 curl --fail --location --silent https://get.k3s.io | sh -s - --disable traefik
-sudo kubectl wait --all --for condition=Ready --timeout 60s node'
+
+while ! sudo kubectl wait --all --for condition=Ready node; do
+  sleep 1s
+done'
 
 ssh -F "${KEREK_SSH_CONFIGURATION}" "${KEREK_SSH_HOST}" -- \
   sudo cat /etc/rancher/k3s/k3s.yaml >"${KUBECONFIG}"

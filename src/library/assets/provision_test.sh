31f9e806c24662755e45abe4339aa365a62a72d8
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-12-26 16:20:14 +0100

    Revise naming for clarity

--- src/library/assets/provision_test.sh
+++ src/library/assets/provision_test.sh
@@ -6,13 +6,13 @@ set -o pipefail
 
 set -o xtrace
 
-test_package_management_setup() {
+test_package_management() {
   [[ ! -f /var/run/reboot-required ]]
 
   systemctl is-active unattended-upgrades.service
 }
 
-test_user_setup() {
+test_user_management() {
   local -r sshd_configuration="$(sudo sshd -T)"
   echo "${sshd_configuration}" | grep '^passwordauthentication no$'
   echo "${sshd_configuration}" | grep '^permitrootlogin no$'
@@ -20,13 +20,13 @@ test_user_setup() {
   [[ "$(groups kerek)" == 'kerek : kerek' ]]
 }
 
-test_podman_setup() {
+test_container_engine() {
   podman --version
   loginctl show-user kerek | grep '^Linger=yes$'
   [[ "$(sysctl --values net.ipv4.ip_unprivileged_port_start)" == 80 ]]
 }
 
-test_firewall_setup() {
+test_firewall() {
   diff --ignore-trailing-space <(sudo ufw status verbose) <(echo 'Status: active
 Logging: on (low)
 Default: deny (incoming), allow (outgoing), disabled (routed)
@@ -44,10 +44,10 @@ To                         Action      From
 }
 
 main() {
-  test_package_management_setup
-  test_user_setup
-  test_podman_setup
-  test_firewall_setup
+  test_package_management
+  test_user_management
+  test_container_engine
+  test_firewall
 }
 
 main "$@"

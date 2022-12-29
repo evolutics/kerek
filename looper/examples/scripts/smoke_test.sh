7d009dc9567071da98b8fbfbb5c1cdbe88b137cf
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-12-29 21:25:31 +0100

    Fix bug to let Ansible distinguish hosts
    Otherwise, our tests provision production twice but never staging. It
    seems that Ansible only distinguishes hosts by inventory name instead of
    SSH configuration file. Hence, we must give the hosts different names.

--- looper/examples/scripts/smoke_test.sh
+++ looper/examples/scripts/smoke_test.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "Smoke tests: ${KEREK_ENVIRONMENT_ID}" >>log.txt
+echo "Smoke tests: ${KEREK_SSH_HOST}" >>log.txt
 
 curl --fail --max-time 3 --retry 99 --retry-connrefused --retry-max-time 150 \
   --show-error http://"${KEREK_IP_ADDRESS}"

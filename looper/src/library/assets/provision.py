203f2bc20fa79331e456e8e4188eeb67c1ba1571
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-12-29 19:19:52 +0100

    Move remaining provisioning to Ansible integration for simplicity
    Be more fine grained than doing a general system reboot to avoid
    downtime on each provision call. This also speeds up the tests.
    
    In addition, use the existing retry mechanism of Ansible.

--- looper/src/library/assets/provision.py
+++ looper/src/library/assets/provision.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
 
-import datetime
 import os
 import pathlib
 import shlex
 import subprocess
-import time
 
 
 def main():
-    print("Doing provisioning.")
-    _do_provisioning()
-    print("Rebooting.")
-    _reboot()
-    print("Testing provisioning.")
-    _test_provisioning()
-
-
-def _do_provisioning():
     scripts_folder = pathlib.Path(os.environ["KEREK_CACHE_SCRIPTS"])
     subprocess.run(
         [
@@ -30,53 +19,6 @@ def _do_provisioning():
             scripts_folder / "playbook.yaml",
         ],
         check=True,
-    )
-
-
-def _reboot():
-    subprocess.run(
-        [
-            "ssh",
-            "-F",
-            os.environ["KEREK_SSH_CONFIGURATION"],
-            "-f",
-            os.environ["KEREK_SSH_HOST"],
-            "--",
-            "sudo",
-            "reboot",
-        ],
-        check=True,
-    )
-
-
-def _test_provisioning():
-    timeout = datetime.timedelta(seconds=5)
-
-    while True:
-        try:
-            return _try_to_test_provisioning(timeout)
-        except subprocess.CalledProcessError:
-            pass
-        except subprocess.TimeoutExpired:
-            timeout *= 2
-        time.sleep(datetime.timedelta(seconds=1).total_seconds())
-
-
-def _try_to_test_provisioning(timeout):
-    scripts_folder = pathlib.Path(os.environ["KEREK_CACHE_SCRIPTS"])
-    subprocess.run(
-        [
-            "ssh",
-            "-F",
-            os.environ["KEREK_SSH_CONFIGURATION"],
-            os.environ["KEREK_SSH_HOST"],
-            "--",
-            f"KEREK_DEPLOY_USER={os.environ['KEREK_DEPLOY_USER']}",
-            "bash",
-        ],
-        check=True,
-        input=(scripts_folder / "provision_test.sh").read_bytes(),
-        timeout=timeout.total_seconds(),
     )
 
 

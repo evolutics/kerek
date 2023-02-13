a510155346db7d116ebca615331115e99cbe3f99
Benjamin Fischer <benjamin.fischer@evolutics.info>
2023-02-13 22:37:50 +0100

    Group SSH arguments for clarity

--- src/subcommand/deploy/mod.rs
+++ src/subcommand/deploy/mod.rs
@@ -16,9 +16,11 @@ pub fn go(in_: In) -> anyhow::Result<()> {
         Some(ssh_host) => deploy_remotely(
             &in_.compose_file,
             &compose,
-            &in_.ssh_configuration,
-            &ssh_host,
-            &in_.ssh_user,
+            &Ssh {
+                configuration: in_.ssh_configuration,
+                host: ssh_host,
+                user: in_.ssh_user,
+            },
         ),
     }
 }
@@ -30,19 +32,23 @@ pub struct In {
     pub ssh_user: Option<String>,
 }
 
+struct Ssh {
+    configuration: Option<path::PathBuf>,
+    host: String,
+    user: Option<String>,
+}
+
 fn deploy_remotely(
     compose_file: &path::Path,
     compose: &compose::Main,
-    ssh_configuration: &Option<path::PathBuf>,
-    ssh_host: &str,
-    ssh_user: &Option<String>,
+    ssh: &Ssh,
 ) -> anyhow::Result<()> {
     println!("Assembling artifacts.");
     assemble_artifacts(compose_file, compose)?;
     println!("Synchronizing artifacts.");
-    synchronize_artifacts(compose, ssh_configuration, ssh_host, ssh_user)?;
+    synchronize_artifacts(compose, ssh)?;
     println!("Deploying on remote.");
-    run_deploy_on_remote(compose, ssh_configuration, ssh_host, ssh_user)
+    run_deploy_on_remote(compose, ssh)
 }
 
 fn assemble_artifacts(compose_file: &path::Path, compose: &compose::Main) -> anyhow::Result<()> {
@@ -53,22 +59,17 @@ fn assemble_artifacts(compose_file: &path::Path, compose: &compose::Main) -> any
     Ok(())
 }
 
-fn synchronize_artifacts(
-    compose: &compose::Main,
-    ssh_configuration: &Option<path::PathBuf>,
-    ssh_host: &str,
-    ssh_user: &Option<String>,
-) -> anyhow::Result<()> {
+fn synchronize_artifacts(compose: &compose::Main, ssh: &Ssh) -> anyhow::Result<()> {
     let mut source = ffi::OsString::from(&compose.x_wheelsticks.local_workbench);
     source.push("/");
     let source = source;
 
     let mut destination = ffi::OsString::new();
-    if let Some(ssh_user) = ssh_user {
-        destination.push(ssh_user);
+    if let Some(user) = &ssh.user {
+        destination.push(user);
         destination.push("@");
     }
-    destination.push(ssh_host);
+    destination.push(&ssh.host);
     destination.push(":");
     destination.push(&compose.x_wheelsticks.remote_workbench);
     let destination = destination;
@@ -76,27 +77,26 @@ fn synchronize_artifacts(
     command::status_ok(
         process::Command::new("rsync")
             .args(["--archive", "--delete"])
-            .args(ssh_configuration.iter().flat_map(|ssh_configuration| {
-                ["--rsh".into(), format!("ssh -F {ssh_configuration:?}")]
-            }))
+            .args(
+                ssh.configuration.iter().flat_map(|configuration| {
+                    ["--rsh".into(), format!("ssh -F {configuration:?}")]
+                }),
+            )
             .arg("--")
             .args([source, destination]),
     )
 }
 
-fn run_deploy_on_remote(
-    compose: &compose::Main,
-    ssh_configuration: &Option<path::PathBuf>,
-    ssh_host: &str,
-    ssh_user: &Option<String>,
-) -> anyhow::Result<()> {
+fn run_deploy_on_remote(compose: &compose::Main, ssh: &Ssh) -> anyhow::Result<()> {
     command::status_ok(
         process::Command::new("ssh")
-            .args(ssh_configuration.iter().flat_map(|ssh_configuration| {
-                [ffi::OsStr::new("-F"), ssh_configuration.as_os_str()]
-            }))
-            .args(ssh_user.iter().flat_map(|ssh_user| ["-l", ssh_user]))
-            .args([ssh_host, "--", "wheelsticks", "deploy", "--compose-file"])
+            .args(
+                ssh.configuration
+                    .iter()
+                    .flat_map(|configuration| [ffi::OsStr::new("-F"), configuration.as_os_str()]),
+            )
+            .args(ssh.user.iter().flat_map(|user| ["-l", user]))
+            .args([&ssh.host, "--", "wheelsticks", "deploy", "--compose-file"])
             .arg(compose.x_wheelsticks.remote_workbench.join("compose.yaml")),
     )
 }

5fc20ce556c3b2884375e8fcf50962f474104727
Benjamin Fischer <benjamin.fischer@evolutics.info>
2023-02-13 22:27:03 +0100

    Use Ansible playbook variables for clarity

--- src/subcommand/provision/mod.rs
+++ src/subcommand/provision/mod.rs
@@ -17,7 +17,17 @@ pub fn go(in_: In) -> anyhow::Result<()> {
 
     command::status_ok(
         process::Command::new("ansible-playbook")
-            .args(["--inventory", &format!(",{ssh_host}")])
+            .args([
+                "--extra-vars",
+                &serde_json::to_string(&PlaybookVariables {
+                    deploy_user: &in_.deploy_user,
+                    own_executable: &env::current_exe()
+                        .context("Unable to get current executable.")?,
+                    provision_test: provision_test.as_ref(),
+                })?,
+                "--inventory",
+                &format!(",{ssh_host}"),
+            ])
             .args(in_.ssh_configuration.iter().flat_map(|ssh_configuration| {
                 [
                     "--ssh-common-args".into(),
@@ -25,13 +35,7 @@ pub fn go(in_: In) -> anyhow::Result<()> {
                 ]
             }))
             .arg("--")
-            .arg(playbook.as_ref())
-            .env("WHEELSTICKS_DEPLOY_USER", in_.deploy_user)
-            .env(
-                "WHEELSTICKS_EXECUTABLE",
-                env::current_exe().context("Unable to get current executable.")?,
-            )
-            .env("WHEELSTICKS_PROVISION_TEST", provision_test.as_ref()),
+            .arg(playbook.as_ref()),
     )
 }
 
@@ -39,4 +43,11 @@ pub struct In {
     pub deploy_user: String,
     pub ssh_configuration: Option<path::PathBuf>,
     pub ssh_host: String,
+}
+
+#[derive(serde::Serialize)]
+struct PlaybookVariables<'a> {
+    deploy_user: &'a str,
+    own_executable: &'a path::Path,
+    provision_test: &'a path::Path,
 }

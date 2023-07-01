e38d8a1e3a16147eed052274655672d65a63f349
Benjamin Fischer <benjamin.fischer@evolutics.info>
2023-07-01 11:38:51 +0200

    Share environment construction to avoid repetition

--- tests/handles_example.rs
+++ tests/handles_example.rs
@@ -62,25 +62,20 @@ const SSH_HOST: &str = "example";
 const VM_IP_ADDRESS: &str = "192.168.60.97";
 
 fn assert_command_in_context(command: &mut process::Command) -> anyhow::Result<()> {
-    assert!(command.current_dir(FOLDER).status()?.success());
+    assert!(command
+        .current_dir(FOLDER)
+        .env("VM_IP_ADDRESS", VM_IP_ADDRESS)
+        .status()?
+        .success());
     Ok(())
 }
 
 fn reset_vm() -> anyhow::Result<()> {
-    assert_command_in_context(
-        process::Command::new("vagrant")
-            .args(["destroy", "--force"])
-            .env("VM_IP_ADDRESS", VM_IP_ADDRESS),
-    )?;
-    assert_command_in_context(
-        process::Command::new("vagrant")
-            .arg("up")
-            .env("VM_IP_ADDRESS", VM_IP_ADDRESS),
-    )?;
+    assert_command_in_context(process::Command::new("vagrant").args(["destroy", "--force"]))?;
+    assert_command_in_context(process::Command::new("vagrant").arg("up"))?;
     assert_command_in_context(
         process::Command::new("vagrant")
             .args(["ssh-config", "--host", SSH_HOST])
-            .env("VM_IP_ADDRESS", VM_IP_ADDRESS)
             .stdout(fs::File::create(
                 path::Path::new(FOLDER).join(SSH_CONFIGURATION_FILE),
             )?),

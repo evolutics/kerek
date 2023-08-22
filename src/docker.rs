8a85b4a4469069f069b0d76d34a56290e234fcba
Benjamin Fischer <benjamin.fischer@evolutics.info>
2023-08-22 22:10:18 +0200

    Support remaining top-level Docker arguments

--- src/docker.rs
+++ src/docker.rs
@@ -1,48 +1,104 @@
 use std::process;
 
 pub struct Cli {
+    config: Option<String>,
     context: Option<String>,
+    debug: bool,
     file: Vec<String>,
     host: Option<String>,
+    log_level: Option<String>,
     project_directory: Option<String>,
     project_name: Option<String>,
+    tls: bool,
+    tlscacert: Option<String>,
+    tlscert: Option<String>,
+    tlskey: Option<String>,
+    tlsverify: bool,
 }
 
 pub struct In {
+    pub config: Option<String>,
     pub context: Option<String>,
+    pub debug: bool,
     pub file: Vec<String>,
     pub host: Option<String>,
+    pub log_level: Option<String>,
     pub project_directory: Option<String>,
     pub project_name: Option<String>,
+    pub tls: bool,
+    pub tlscacert: Option<String>,
+    pub tlscert: Option<String>,
+    pub tlskey: Option<String>,
+    pub tlsverify: bool,
 }
 
 impl Cli {
     pub fn new(
         In {
+            config,
             context,
+            debug,
             file,
             host,
+            log_level,
             project_directory,
             project_name,
+            tls,
+            tlscacert,
+            tlscert,
+            tlskey,
+            tlsverify,
         }: In,
     ) -> Self {
         Cli {
+            config,
             context,
+            debug,
             file,
             host,
+            log_level,
             project_directory,
             project_name,
+            tls,
+            tlscacert,
+            tlscert,
+            tlskey,
+            tlsverify,
         }
     }
 
     pub fn docker(&self) -> process::Command {
         let mut command = process::Command::new("docker");
 
+        if let Some(config) = &self.config {
+            command.args(["--config", config]);
+        }
         if let Some(context) = &self.context {
             command.args(["--context", context]);
         }
+        if self.debug {
+            command.arg("--debug");
+        }
         if let Some(host) = &self.host {
             command.args(["--host", host]);
+        }
+        if let Some(log_level) = &self.log_level {
+            command.args(["--log-level", log_level]);
+        }
+        if self.tls {
+            command.arg("--tls");
+        }
+        if let Some(tlscacert) = &self.tlscacert {
+            command.args(["--tlscacert", tlscacert]);
+        }
+        if let Some(tlscert) = &self.tlscert {
+            command.args(["--tlscert", tlscert]);
+        }
+        if let Some(tlskey) = &self.tlskey {
+            command.args(["--tlskey", tlskey]);
+        }
+        if self.tlsverify {
+            command.arg("--tlsverify");
         }
 
         command
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,23 @@ mod deploy;
 mod docker;
 
 use clap::Parser;
+use clap::ValueEnum;
 
 fn main() -> anyhow::Result<()> {
     let Cli {
-        docker_arguments: DockerArguments { context, host },
+        docker_arguments:
+            DockerArguments {
+                config,
+                context,
+                debug,
+                host,
+                log_level,
+                tls,
+                tlscacert,
+                tlscert,
+                tlskey,
+                tlsverify,
+            },
         subcommand,
     } = Cli::parse();
 
@@ -20,11 +33,21 @@ fn main() -> anyhow::Result<()> {
                 },
         } => deploy::go(deploy::In {
             docker_cli: docker::Cli::new(docker::In {
+                config,
                 context,
+                debug,
                 file,
                 host,
+                log_level: log_level
+                    .and_then(|level| level.to_possible_value())
+                    .map(|level| level.get_name().into()),
                 project_directory,
                 project_name,
+                tls,
+                tlscacert,
+                tlscert,
+                tlskey,
+                tlsverify,
             }),
         }),
     }
@@ -42,14 +65,55 @@ struct Cli {
 
 #[derive(clap::Args)]
 struct DockerArguments {
+    /// Location of client config files
+    #[arg(long)]
+    config: Option<String>,
+
     /// Name of the context to use to connect to the daemon (overrides
     /// DOCKER_HOST env var and default context set with "docker context use")
     #[arg(long, short = 'c')]
     context: Option<String>,
 
+    /// Enable debug mode
+    #[arg(long, short = 'D')]
+    debug: bool,
+
     /// Daemon socket to connect to
     #[arg(long, short = 'H')]
     host: Option<String>,
+
+    /// Set the logging level
+    #[arg(long, short = 'l', value_enum)]
+    log_level: Option<LogLevel>,
+
+    /// Use TLS; implied by --tlsverify
+    #[arg(long)]
+    tls: bool,
+
+    /// Trust certs signed only by this CA
+    #[arg(long)]
+    tlscacert: Option<String>,
+
+    /// Path to TLS certificate file
+    #[arg(long)]
+    tlscert: Option<String>,
+
+    /// Path to TLS key file
+    #[arg(long)]
+    tlskey: Option<String>,
+
+    /// Use TLS and verify the remote
+    #[arg(long)]
+    tlsverify: bool,
+}
+
+#[derive(Clone, ValueEnum)]
+enum LogLevel {
+    Debug,
+    Info,
+    Warn,
+    Error,
+    Fatal,
 }
 
 #[derive(clap::Subcommand)]
@@ -61,7 +125,6 @@ enum Subcommand {
     // TODO: Support maintaining systemd units.
     // TODO: Support more Docker Compose `up` arguments, e.g. `--build`.
     // TODO: Support more Docker Compose standard arguments, e.g. `--env-file`.
-    // TODO: Support more Docker standard arguments, e.g. `--context`.
     // TODO: Support use as plugin (https://github.com/docker/cli/issues/1534).
     Deploy {
         #[command(flatten)]

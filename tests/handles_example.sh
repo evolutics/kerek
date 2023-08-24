5e47f8cd399555f248c35bfba201c7b62007cc63
Benjamin Fischer <benjamin.fischer@evolutics.info>
2023-08-24 14:34:50 +0200

    Give Podman API service time to start

--- tests/handles_example.sh
+++ tests/handles_example.sh
@@ -24,6 +24,7 @@ main() {
   podman system service --time 0 "${DOCKER_HOST}" &
   # shellcheck disable=SC2064
   trap "kill -SIGINT $!" EXIT
+  sleep 2s
 
   docker compose build
   docker --host "${DOCKER_HOST}" save example-web \

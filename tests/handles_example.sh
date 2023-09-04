8de748b241d989334e23f36a9682111239e3af12
Benjamin Fischer <benjamin.fischer@evolutics.info>
2023-09-04 22:12:57 +0200

    Test both supported container engines: Docker and Podman

--- tests/handles_example.sh
+++ tests/handles_example.sh
@@ -4,23 +4,36 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-main() {
-  export DOCKER_HOST="unix://${PWD}/podman.sock"
+test_container_engine() {
+  local -r container_engine="$1"
 
   cd example
-
-  podman system service --time 0 "${DOCKER_HOST}" &
-  # shellcheck disable=SC2064
-  trap "kill -SIGINT $!" EXIT
-  sleep 2s
 
   docker compose down
   docker compose build
 
-  "${WHEELSTICKS}" --container-engine podman deploy
+  "${WHEELSTICKS}" --container-engine "${container_engine}" deploy
 
   curl --fail --max-time 3 --retry 99 --retry-connrefused --retry-max-time 15 \
     --show-error http://localhost:8080
+
+  docker compose down
+}
+
+main() {
+  (
+    test_container_engine docker
+  )
+
+  (
+    export DOCKER_HOST="unix://${PWD}/podman.sock"
+    podman system service --time 0 "${DOCKER_HOST}" &
+    # shellcheck disable=SC2064
+    trap "kill -SIGINT $!" EXIT
+    sleep 2s
+
+    test_container_engine podman
+  )
 }
 
 main "$@"

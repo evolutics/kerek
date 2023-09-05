0269475e7ea7c1dd2d31cac76fec119d275b8bfa
Benjamin Fischer <benjamin.fischer@evolutics.info>
2023-09-05 19:58:24 +0200

    Remove dead code

--- src/command.rs
+++ src/command.rs
@@ -2,19 +2,6 @@ use anyhow::Context;
 use serde::de;
 use std::io;
 use std::process;
-use std::time;
-use wait_timeout::ChildExt;
-
-#[allow(dead_code)]
-pub fn status_bit(command: &mut process::Command) -> anyhow::Result<bool> {
-    go(command, process::Command::status, |status| {
-        match status.code() {
-            Some(0) => Ok(false),
-            Some(1) => Ok(true),
-            _ => status_error(status),
-        }
-    })
-}
 
 pub fn status_ok(command: &mut process::Command) -> anyhow::Result<()> {
     go(command, process::Command::status, |status| {
@@ -26,79 +13,10 @@ pub fn status_ok(command: &mut process::Command) -> anyhow::Result<()> {
     })
 }
 
-#[allow(dead_code)]
-pub fn status_within_time(
-    command: &mut process::Command,
-    timeout: time::Duration,
-) -> anyhow::Result<StatusWithinTime> {
-    go(
-        command,
-        |command| {
-            let mut child = command.spawn()?;
-            let status = child.wait_timeout(timeout)?;
-            Ok((child, status))
-        },
-        |(mut child, status)| {
-            Ok(match status {
-                None => {
-                    let _ = child.kill();
-                    let _ = child.wait();
-                    StatusWithinTime::Timeout
-                }
-                Some(status) => {
-                    if status.success() {
-                        StatusWithinTime::Success
-                    } else {
-                        StatusWithinTime::Failure
-                    }
-                }
-            })
-        },
-    )
-}
-
-#[derive(Debug, PartialEq)]
-pub enum StatusWithinTime {
-    Failure,
-    Success,
-    Timeout,
-}
-
 pub fn stdout_json<T: de::DeserializeOwned>(command: &mut process::Command) -> anyhow::Result<T> {
     go(command, process::Command::output, |output| {
         if output.status.success() {
             serde_json::from_slice(&output.stdout).context("Unable to deserialize JSON from stdout")
-        } else {
-            status_error(output.status)
-        }
-    })
-}
-
-#[allow(dead_code)]
-pub fn stdout_jsons<T: de::DeserializeOwned>(
-    command: &mut process::Command,
-) -> anyhow::Result<Vec<T>> {
-    go(command, process::Command::output, |output| {
-        if output.status.success() {
-            let stream = serde_json::Deserializer::from_slice(&output.stdout).into_iter();
-            let mut values = vec![];
-            for (index, value) in stream.enumerate() {
-                let value = value
-                    .with_context(|| format!("Unable to deserialize JSON #{index} from stdout"))?;
-                values.push(value);
-            }
-            Ok(values)
-        } else {
-            status_error(output.status)
-        }
-    })
-}
-
-#[allow(dead_code)]
-pub fn stdout_raw(command: &mut process::Command) -> anyhow::Result<Vec<u8>> {
-    go(command, process::Command::output, |output| {
-        if output.status.success() {
-            Ok(output.stdout)
         } else {
             status_error(output.status)
         }
@@ -140,14 +58,6 @@ fn status_error<T>(status: process::ExitStatus) -> anyhow::Result<T> {
 mod tests {
     use super::*;
 
-    #[test_case::test_case(invalid_program_(), None; "invalid program")]
-    #[test_case::test_case(shell("exit 0"), Some(false); "zero")]
-    #[test_case::test_case(shell("exit 1"), Some(true); "one")]
-    #[test_case::test_case(shell("exit 2"), None; "other")]
-    fn status_bit_handles(mut command: process::Command, expected: Option<bool>) {
-        assert_eq!(status_bit(&mut command).ok(), expected)
-    }
-
     #[test_case::test_case(invalid_program_(), false; "invalid program")]
     #[test_case::test_case(shell("exit 0"), true; "success")]
     #[test_case::test_case(shell("exit 1"), false; "failure")]
@@ -156,38 +66,10 @@ mod tests {
     }
 
     #[test_case::test_case(invalid_program_(), None; "invalid program")]
-    #[test_case::test_case(shell("exit 0"), Some(StatusWithinTime::Success); "success")]
-    #[test_case::test_case(shell("exit 1"), Some(StatusWithinTime::Failure); "failure")]
-    #[test_case::test_case(shell("sleep 5"), Some(StatusWithinTime::Timeout); "timeout")]
-    fn status_within_time_handles(
-        mut command: process::Command,
-        expected: Option<StatusWithinTime>,
-    ) {
-        assert_eq!(
-            status_within_time(&mut command, time::Duration::from_secs_f32(0.01)).ok(),
-            expected,
-        )
-    }
-
-    #[test_case::test_case(invalid_program_(), None; "invalid program")]
     #[test_case::test_case(shell("exit 1"), None; "failure")]
     #[test_case::test_case(shell("printf '\"Hi\"'"), Some("Hi".into()); "success")]
     fn stdout_json_handles(mut command: process::Command, expected: Option<String>) {
         assert_eq!(stdout_json(&mut command).ok(), expected)
-    }
-
-    #[test_case::test_case(invalid_program_(), None; "invalid program")]
-    #[test_case::test_case(shell("exit 1"), None; "failure")]
-    #[test_case::test_case(shell("printf '3 5'"), Some(vec![3, 5]); "success")]
-    fn stdout_jsons_handles(mut command: process::Command, expected: Option<Vec<i8>>) {
-        assert_eq!(stdout_jsons(&mut command).ok(), expected)
-    }
-
-    #[test_case::test_case(invalid_program_(), None; "invalid program")]
-    #[test_case::test_case(shell("exit 1"), None; "failure")]
-    #[test_case::test_case(shell("printf Hi"), Some("Hi".into()); "success")]
-    fn stdout_raw_handles(mut command: process::Command, expected: Option<Vec<u8>>) {
-        assert_eq!(stdout_raw(&mut command).ok(), expected)
     }
 
     #[test_case::test_case(invalid_program_(), None; "invalid program")]

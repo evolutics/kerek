debc1dc87076f3445fab532df53723ddf3af2b39
Benjamin Fischer <benjamin.fischer@evolutics.info>
2023-09-17 18:16:12 +0200

    Support copying for simpler usage

--- src/log.rs
+++ src/log.rs
@@ -2,7 +2,7 @@ use std::sync;
 
 static LEVEL: sync::OnceLock<Level> = sync::OnceLock::new();
 
-#[derive(Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Copy, Eq, Ord, PartialEq, PartialOrd)]
 pub enum Level {
     Debug,
     Info,
@@ -11,8 +11,8 @@ pub enum Level {
     Fatal,
 }
 
-pub fn level() -> &'static Level {
-    LEVEL.get().unwrap_or(&Level::Debug)
+pub fn level() -> Level {
+    *LEVEL.get().unwrap_or(&Level::Debug)
 }
 
 pub fn set_level(level: Level) -> anyhow::Result<()> {
@@ -24,7 +24,7 @@ pub fn set_level(level: Level) -> anyhow::Result<()> {
 #[macro_export]
 macro_rules! info {
     ($($argument:tt)*) => {{
-        if $crate::log::level() <= &$crate::log::Level::Info {
+        if $crate::log::level() <= $crate::log::Level::Info {
             eprintln!($($argument)*);
         }
     }};

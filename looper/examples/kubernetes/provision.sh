644f511f3c1dae6c85764e190af277428a4a0e6f
Benjamin Fischer <benjamin.fischer@evolutics.info>
2024-05-11 19:54:42 +0200

    Fix K3s error about certificate not valid for VM IP
    Error message:
    
    ```
    Get "https://192.168.60.160:6443/version":
    tls: failed to verify certificate: x509: certificate is valid for
    10.43.0.1, 127.0.0.1, 192.168.121.102, ::1, not 192.168.60.160
    ```
    
    See https://github.com/k3s-io/k3s/issues/1381 for more context.

--- looper/examples/kubernetes/provision.sh
+++ looper/examples/kubernetes/provision.sh
@@ -4,12 +4,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-ssh -F "${KEREK_SSH_CONFIGURATION}" "${KEREK_SSH_HOST}" -- '
-curl --fail --location --silent https://get.k3s.io | sh -s - --disable traefik
+ssh -F "${KEREK_SSH_CONFIGURATION}" "${KEREK_SSH_HOST}" \
+  KEREK_IP_ADDRESS="${KEREK_IP_ADDRESS}" 'bash -s' <<'EOF'
+export INSTALL_K3S_EXEC="--disable traefik --tls-san ${KEREK_IP_ADDRESS}"
+curl --fail --location --silent https://get.k3s.io | sh -
 
 while ! sudo kubectl wait --all --for condition=Ready node; do
   sleep 1s
-done'
+done
+EOF
 
 ssh -F "${KEREK_SSH_CONFIGURATION}" "${KEREK_SSH_HOST}" -- \
   sudo cat /etc/rancher/k3s/k3s.yaml >"${KUBECONFIG}"

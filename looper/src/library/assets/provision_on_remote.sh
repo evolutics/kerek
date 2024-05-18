f95dc192b8bee9c4407627c84ad45e5a7e80e6e1
Benjamin Fischer <benjamin.fischer@evolutics.info>
2024-05-18 12:30:43 +0200

    Prune containers, images, and networks

--- looper/src/library/assets/provision_on_remote.sh
+++ looper/src/library/assets/provision_on_remote.sh
@@ -12,3 +12,5 @@ sudo ln --force --symbolic /usr/bin/podman /usr/local/bin/docker
 
 sudo sysctl --write net.ipv4.ip_unprivileged_port_start=80 \
   | sudo tee /etc/sysctl.d/99-custom.conf
+
+podman system prune --all --filter until=720h --force

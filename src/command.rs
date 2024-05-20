bd309495abd8506f114c5d613e3717d0c5011983
Benjamin Fischer <benjamin.fischer@evolutics.info>
2024-05-20 20:25:18 +0200

    Quote human text as per style guide

--- src/command.rs
+++ src/command.rs
@@ -162,7 +162,7 @@ mod tests {
 
     #[test_case::test_case(invalid_program_(), None; "invalid program")]
     #[test_case::test_case(bash("exit 1"), None; "failure")]
-    #[test_case::test_case(bash("printf Hi"), Some("Hi".into()); "success")]
+    #[test_case::test_case(bash("printf 'Hi'"), Some("Hi".into()); "success")]
     fn stdout_utf8_handles(mut command: process::Command, expected: Option<String>) {
         assert_eq!(stdout_utf8(&mut command).ok(), expected)
     }

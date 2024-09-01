7e168ccc1aeb040d26ed30a0be12c30283d243de
Benjamin Fischer <benjamin.fischer@evolutics.info>
2024-09-01 17:00:05 +0200

    Clean up temporary file for custom SSH config

--- examples/ssh_delivery/test.sh
+++ examples/ssh_delivery/test.sh
@@ -9,24 +9,28 @@ main() {
   trap 'vagrant destroy --force' EXIT
   vagrant up
 
-  vagrant ssh-config --host ssh-host >ssh_config
+  (
+    trap 'rm --force ssh_config' EXIT
+    vagrant ssh-config --host ssh-host >ssh_config
 
-  "${KEREK}" provision --force --ssh-config ssh_config ssh-host
+    "${KEREK}" provision --force --ssh-config ssh_config ssh-host
 
-  docker compose pull --ignore-buildable
+    docker compose pull --ignore-buildable
 
-  docker compose config --images | "${KEREK}" run-with-ssh-config -- \
-    ssh_config "${KEREK}" --host ssh://ssh-host transfer-images -
+    docker compose config --images | "${KEREK}" run-with-ssh-config -- \
+      ssh_config "${KEREK}" --host ssh://ssh-host transfer-images -
 
-  "${KEREK}" run-with-ssh-config -- ssh_config "${KEREK}" \
-    --host ssh://ssh-host deploy --no-build --pull never --remove-orphans --wait
+    "${KEREK}" run-with-ssh-config -- ssh_config "${KEREK}" \
+      --host ssh://ssh-host deploy --no-build --pull never --remove-orphans \
+      --wait
 
-  local -r result="$(curl --fail-with-body --max-time 3 --retry 99 \
-    --retry-connrefused --retry-max-time 150 http://192.168.60.159)"
-  if [[ "${result}" != 'hello-world' ]]; then
-    echo "Unexpected result of sanity check: ${result}" >&2
-    exit 1
-  fi
+    local -r result="$(curl --fail-with-body --max-time 3 --retry 99 \
+      --retry-connrefused --retry-max-time 150 http://192.168.60.159)"
+    if [[ "${result}" != 'hello-world' ]]; then
+      echo "Unexpected result of sanity check: ${result}" >&2
+      exit 1
+    fi
+  )
 }
 
 main "$@"

1b1f83def85adf5da5e8df5d2fad8b03586db92d
Benjamin Fischer <benjamin.fischer@evolutics.info>
2024-09-21 11:01:00 +0200

    Flatten command logic using command context

--- src/command.rs
+++ src/command.rs
@@ -1,6 +1,5 @@
 use anyhow::Context;
 use serde::de;
-use std::io;
 use std::io::Write;
 use std::process;
 use std::thread;
@@ -12,116 +11,96 @@ pub fn piped_ok(
     let mut child = writer
         .stdout(process::Stdio::piped())
         .spawn()
-        .with_context(|| format!("Unable to spawn command: {writer:?}"))?;
+        .context("Unable to spawn")
+        .command_context(writer)?;
     let stdout = child
         .stdout
         .take()
-        .with_context(|| format!("Unable to open stdout from command: {writer:?}"))?;
+        .context("Unable to open stdout")
+        .command_context(writer)?;
 
     let last_result = status_ok(reader.stdin(stdout));
 
-    match child
-        .try_wait()
-        .with_context(|| format!("Unable to try waiting for command: {writer:?}"))?
-    {
-        None => child
-            .kill()
-            .with_context(|| format!("Unable to kill command: {writer:?}")),
-        Some(status) => {
-            status_result(status).with_context(|| format!("Error with command: {writer:?}"))
-        }
-    }?;
+    (|| match child.try_wait().context("Unable to try waiting")? {
+        None => child.kill().context("Unable to kill"),
+        Some(status) => status_result(status),
+    })()
+    .command_context(writer)?;
 
     last_result
 }
 
 pub fn status_ok(command: &mut process::Command) -> anyhow::Result<()> {
-    go(command, process::Command::status, status_result)
+    (|| status_result(command.status()?))().command_context(command)
 }
 
 pub fn stdin_ok(input: &'static [u8], command: &mut process::Command) -> anyhow::Result<()> {
-    go(
-        command.stdin(process::Stdio::piped()),
-        process::Command::spawn,
-        |mut child| {
-            let mut stdin = child.stdin.take().context("Unable to open stdin")?;
-            thread::spawn(move || stdin.write_all(input).context("Unable to write to stdin"));
-            status_result(child.wait().context("Unable to wait")?)
-        },
-    )
+    (|| {
+        let mut child = command.stdin(process::Stdio::piped()).spawn()?;
+        let mut stdin = child.stdin.take().context("Unable to open stdin")?;
+        thread::spawn(move || stdin.write_all(input).context("Unable to write to stdin"));
+        status_result(child.wait().context("Unable to wait")?)
+    })()
+    .command_context(command)
 }
 
 pub fn stdout_json<T: de::DeserializeOwned>(command: &mut process::Command) -> anyhow::Result<T> {
-    go(
-        command.stderr(process::Stdio::inherit()),
-        process::Command::output,
-        |output| {
-            status_result(output.status)?;
-            serde_json::from_slice(&output.stdout).context("Unable to deserialize JSON from stdout")
-        },
-    )
+    (|| {
+        let output = command.stderr(process::Stdio::inherit()).output()?;
+        status_result(output.status)?;
+        serde_json::from_slice(&output.stdout).context("Unable to deserialize JSON from stdout")
+    })()
+    .command_context(command)
 }
 
 pub fn stdout_table<const N: usize>(
     command: &mut process::Command,
 ) -> anyhow::Result<Vec<[String; N]>> {
-    go(
-        command.stderr(process::Stdio::inherit()),
-        process::Command::output,
-        |output| {
-            status_result(output.status)?;
+    (|| {
+        let output = command.stderr(process::Stdio::inherit()).output()?;
+        status_result(output.status)?;
 
-            let table = String::from_utf8(output.stdout).context("Stdout is not valid UTF-8")?;
-            table
-                .lines()
-                .enumerate()
-                .map(|(row_index, row)| {
-                    let fields = row
-                        .split_whitespace()
-                        .map(|field| field.into())
-                        .collect::<Vec<_>>();
+        let table = String::from_utf8(output.stdout).context("Stdout is not valid UTF-8")?;
+        table
+            .lines()
+            .enumerate()
+            .map(|(row_index, row)| {
+                let fields = row
+                    .split_whitespace()
+                    .map(|field| field.into())
+                    .collect::<Vec<_>>();
 
-                    fields.try_into().map_err(|fields: Vec<_>| {
-                        let line_number = row_index + 1;
-                        let field_count = fields.len();
-                        anyhow::anyhow!(
-                            "Unable to parse result line {line_number}, \
-                                expected {N} fields \
-                                but got {field_count}: {row:?}"
-                        )
-                    })
+                fields.try_into().map_err(|fields: Vec<_>| {
+                    let line_number = row_index + 1;
+                    let field_count = fields.len();
+                    anyhow::anyhow!(
+                        "Unable to parse result line {line_number}, \
+                            expected {N} fields \
+                            but got {field_count}: {row:?}"
+                    )
                 })
-                .collect()
-        },
-    )
+            })
+            .collect::<anyhow::Result<_>>()
+    })()
+    .command_context(command)
 }
 
 pub fn stdout_utf8(command: &mut process::Command) -> anyhow::Result<String> {
-    go(
-        command.stderr(process::Stdio::inherit()),
-        process::Command::output,
-        |output| {
-            status_result(output.status)?;
-            String::from_utf8(output.stdout).context("Stdout is not valid UTF-8")
-        },
-    )
+    (|| {
+        let output = command.stderr(process::Stdio::inherit()).output()?;
+        status_result(output.status)?;
+        String::from_utf8(output.stdout).context("Stdout is not valid UTF-8")
+    })()
+    .command_context(command)
 }
 
-fn go<
-    F: FnOnce(&mut process::Command) -> io::Result<T>,
-    G: FnOnce(T) -> anyhow::Result<U>,
-    T,
-    U,
->(
-    command: &mut process::Command,
-    run: F,
-    evaluate: G,
-) -> anyhow::Result<U> {
-    match run(command) {
-        Err(error) => Err(anyhow::anyhow!(error))
-            .with_context(|| format!("Unable to run command: {command:?}")),
-        Ok(value) => evaluate(value)
-            .with_context(|| format!("Unable to evaluate result of command: {command:?}")),
+trait CommandContext<T> {
+    fn command_context(self, command: &process::Command) -> anyhow::Result<T>;
+}
+
+impl<T> CommandContext<T> for anyhow::Result<T> {
+    fn command_context(self, command: &process::Command) -> anyhow::Result<T> {
+        self.with_context(|| format!("Error with command: {command:?}"))
     }
 }
 

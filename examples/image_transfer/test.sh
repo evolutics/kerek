788997a3954c904031b54b4efd68e6b51b416a16
Benjamin Fischer <benjamin.fischer@evolutics.info>
2024-09-28 15:48:51 +0200

    Revise image transfer tests to test forcing, too

--- examples/image_transfer/test.sh
+++ examples/image_transfer/test.sh
@@ -8,22 +8,32 @@ main() {
   local -r box="unix://${PWD}/box.sock"
   CONTAINERS_STORAGE_CONF=storage.toml podman system service --time 0 "${box}" &
   trap 'fuser --kill -TERM box.sock' EXIT
+  podman --url "${box}" system prune --all --force
 
-  mapfile -t compresses < <(shuf --echo -- '' bzip2 gzip xz 'xz -9' zstd)
+  export CONTAINER_ENGINE=podman
+  local -r image='localhost/kerek:latest'
 
-  for compress in "${compresses[@]}"; do
-    echo "Compressing with: '${compress}'"
+  podman build --build-arg VERSION=A --tag "${image}" .
+  kerek --host "${box}" transfer-images -- "${image}"
+  [[ "$(podman --url "${box}" run --rm "${image}" | tee /dev/stderr)" == 'vA' ]]
 
-    podman --host "${box}" rmi --ignore docker.io/busybox
-    [[ -z "$(podman --host "${box}" images --quiet)" ]]
+  podman build --build-arg VERSION=B --tag "${image}" .
+  kerek --host "${box}" transfer-images -- "${image}"
+  [[ "$(podman --url "${box}" run --rm "${image}" | tee /dev/stderr)" == 'vA' ]]
+  kerek --host "${box}" transfer-images --force -- "${image}"
+  [[ "$(podman --url "${box}" run --rm "${image}" | tee /dev/stderr)" == 'vB' ]]
 
-    podman pull docker.io/busybox
-    time kerek --container-engine podman --host "${box}" transfer-images \
-      ${compress:+--compress "${compress}"} docker.io/busybox
-
-    [[ "$(podman --host "${box}" images --format '{{.Repository}}:{{.Tag}}' \
-      | tee /dev/stderr)" == 'docker.io/library/busybox:latest' ]]
+  for compress in $(shuf --echo -- bzip2 gzip xz zstd); do
+    podman build --build-arg "VERSION=${compress}" --tag "${image}" .
+    kerek --host "${box}" transfer-images --force -- "${image}"
+    [[ "$(podman --url "${box}" run --rm "${image}" \
+      | tee /dev/stderr)" == "v${compress}" ]]
   done
+
+  podman build --build-arg VERSION=C --tag "${image}" .
+  kerek --host "${box}" transfer-images --compress 'zstd --verbose' --force -- \
+    "${image}"
+  [[ "$(podman --url "${box}" run --rm "${image}" | tee /dev/stderr)" == 'vC' ]]
 }
 
 main "$@"
